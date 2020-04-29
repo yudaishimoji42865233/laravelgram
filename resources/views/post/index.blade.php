01756e1c3527347b92e65bc938007280c0d87a7a
EC2 Default User <ec2-user@ip-172-31-38-230.us-east-2.compute.internal>
2020-04-29 05:45:24 +0000

    first commit

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+<p>このページは仮のトップページです。</p>
+<a href="#" class="btn btn-primary">仮のボタンです</a>
